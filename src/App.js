c0a0dec257db6f9cce38184b94acf6cb0418d8f0
L. Rommy Arbantas <arbantas@gmail.com>
2019-05-08 22:04:18 +0700

    push lagi ahhh

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 
 function App(props) {
   const [item, setItem] = useState('');
+  const [keranjang, setKeranjang] = useState([]);
+
+  function addItem() {
+    setKeranjang([...keranjang, item]);
+  }
+
   return (
     <div className="App">
       <h2>DAFTAR BELANJA</h2>
       <hr />
 
-      <input
-        type="text" value={item}
-        onChange={e => setItem(e.target.value)}
-      />
-      <button>Tambahkan</button>
+      <input type="text" value={item} onChange={e => setItem(e.target.value)} />
+      <button onClick={addItem}>Tambahkan</button>
 
       <ol>
-        <li>Beras</li>
-        <li>Telur</li>
-        <li>Gula</li>
+        {keranjang.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
       </ol>
     </div>
   );
