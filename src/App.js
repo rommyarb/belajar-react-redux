8635b97e98d4f4ebc3333cd159870b33dbc74b27
L. Rommy Arbantas <arbantas@gmail.com>
2019-05-08 22:45:24 +0700

    pake redux!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
 function App(props) {
-  const [item, setItem] = useState('');
-  const [keranjang, setKeranjang] = useState([]);
-
-  function addItem() {
-    setKeranjang([...keranjang, item]);
-  }
-
   return (
     <div className="App">
       <h2>DAFTAR BELANJA</h2>
       <hr />
 
-      <input type="text" value={item} onChange={e => setItem(e.target.value)} />
-      <button onClick={addItem}>Tambahkan</button>
+      <input
+        type="text"
+        value={props.item}
+        onChange={e => props.setItem(e.target.value)}
+      />
+      <button onClick={props.addItem}>Tambahkan</button>
 
       <ol>
-        {keranjang.map((item, index) => (
+        {props.keranjang.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ol>
@@ -26,4 +24,24 @@ function App(props) {
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    item: state.item,
+    keranjang: state.keranjang
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setItem: value => dispatch({ type: 'SET_ITEM', value }),
+    addItem: () =>
+      dispatch({
+        type: 'ADD_ITEM'
+      })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// REDUCER DAFTAR BELANJA (DB)
+const initialState = {
+  item: '',
+  keranjang: []
+};
+function DB_reducer(state = initialState, action) {
+  // let newState = state; // SALAH
+  let newState = { ...state }; // BENAR
+  if (action.type === 'ADD_ITEM') {
+    newState.keranjang = [...state.keranjang, state.item];
+  } else if (action.type === 'SET_ITEM') {
+    newState.item = action.value;
+  }
+  return newState;
+}
+
+const store = createStore(DB_reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
