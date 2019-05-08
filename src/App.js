8276246c09270dd31ad8e0f45f182552580f973b
L. Rommy Arbantas <arbantas@gmail.com>
2019-05-08 21:39:12 +0700

    'init'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [item, setItem] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>DAFTAR BELANJA</h2>
+      <hr />
+
+      <input
+        type="text" value={item}
+        onChange={e => setItem(e.target.value)}
+      />
+      <button>Tambahkan</button>
+
+      <ol>
+        <li>Beras</li>
+        <li>Telur</li>
+        <li>Gula</li>
+      </ol>
     </div>
   );
 }
